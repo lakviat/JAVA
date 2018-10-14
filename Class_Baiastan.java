756e84633abae31cb4e20eaadd05a2f1ad868230
apple <apple@apples-MacBook-Pro-3.local>
2018-10-14 15:09:00 -0500

    Being transfered

---
+++ Class_Baiastan.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Class_Baiastan {
+    public static void main(String[] args) {
+
+
+        int[][] nums = {{23, 34, 23}, {34, 76, 5}};
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
+
+
+
+
+
+
+
