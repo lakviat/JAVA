eaa3f4771dcd73bd5d07c05fd225a008fc82fd5d
apple <apple@apples-MacBook-Pro-3.local>
2018-10-15 12:42:17 -0500

    Installed

---
+++ Calculator_2D.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Calculator_2D {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j <10 ; j++) {
+                System.out.println("Outer: " + i + " x " + "Inner" + j + " = " + i * j);
+
+            }
+
+        }
+    }
+}
